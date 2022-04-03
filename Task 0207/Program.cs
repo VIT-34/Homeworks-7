7cbc91fe8eb5a59e1c94089879c107644041d1b1
Vitaliy <viurum986@mail.ru>
2022-04-03 18:13:41 +0300

    init  task0207

---
+++ Task 0207/Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+
+
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+
+int[,] array = FillArray(4, 4);
+PrintArray(array);
+int element = Prompt("Введите число => ");
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] == element)
+            Console.WriteLine("Искомый элемент находится: строка {0}, столбец {1}", i, j);
+        
+            
+    }
+    
+}
+Console.WriteLine("Такого элемента нет");
+
+
+
+
