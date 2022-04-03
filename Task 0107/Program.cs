5fa7602478c2519159ddcbd82173641a357b2325
Vitaliy <viurum986@mail.ru>
2022-04-03 17:36:24 +0300

    init  task0107 and .gitignore

---
+++ Task 0107/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+void FillArray(double[,] array)
+{
+        for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble();
+        }
+    }
+    
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = new double[3,4];
+Random random = new Random();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    
+    {
+        array[i, j] = random.NextDouble() * 10;
+        Console.Write("{0:0.0}  ", array[i, j]);
+    }
+    Console.WriteLine();
+}
+
+
+FillArray(array);
+
+
+
+
